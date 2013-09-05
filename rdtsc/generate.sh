ce0a285540c5a99deaa3bb06a9356d081a07fa86
Vince Weaver <vincent.weaver@maine.edu>
2013-09-05 15:51:03 -0400

    add script to make all the runs needed for a final graph

---
+++ rdtsc/generate.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+./generate_results perf_event_rdpmc
+./generate_results perf_event_rdpmc-map_populate
+./generate_results perf_event_rdpmc-touch
+./generate_results perf_event-syscall_static
+./generate_results perf_event-syscall
+./generate_results perf_event-static
+./generate_results perf_event
